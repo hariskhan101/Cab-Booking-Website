015a3de44ca7fab50e115ccdb398993cf22d5184
Haris <hariskhoso@gmail.com>
2023-01-17 21:24:26 +0500

    Initial Commit

---
+++ includes/footer.php
@@ -0,0 +1,45 @@
+
+<footer class="footer-qub">
+	<div class="max-width">
+		<div class="row-social">
+			<div class="title-social">Social Links</div>
+			<div class="links-social">
+			<a href="#" class="links"><img src="img/svg/fb-footer-icon.svg" class="icon-footer"></a>
+			<a href="#" class="links"><img src="img/svg/insta-footer-icon.svg" class="icon-footer"></a>
+			<a href="#" class="links"><img src="img/svg/youtube-footer-icon.svg" class="icon-footer"></a>
+			<a href="#" class="links"><img src="img/svg/twitter-footer-icon.svg" class="icon-footer"></a>
+			</div>
+		</div>
+
+		<div class="row-social">
+			<div style="padding-top: 12px;" class="title-social">Discover Wedrive</div>
+			<div class="links-social">
+			<a href="aboutus.php" class="tags links">About Wedrive</a>
+			<a href="fleet.php" class="tags links">Fleet</a>
+			<a href="drive.php" class="tags links">Drive</a>
+			<!-- <a href="#" class="tags links">Contact Us</a> -->
+			</div>
+			<div class="links-account">
+			<a href="login.php" class="tags links">Login</a>
+			<a href="signUp.php" class="tags links">Signup</a>
+			<a href="booknow.php" class="tags links">Book a ride</a>
+			</div>
+		</div>
+
+		<div class="row-social" style="border: none;padding: 10px 0 10px 0;">
+			<div class="brand-footer">
+			<img src="img/svg/car-solid.svg" class="filter-color qub-filter" class="img-logo">
+			<a href="index.php" class="logo tags links">Wedrive</a></div>
+			<div class="details">
+				<a href="#" class="d-tag links">Terms & conditions</a>
+				<a href="#" class="d-tag links">Privacy Policy</a>
+			</div>
+			<div class="copyrights">
+				<p>Wedrive &copy;<?php echo DATE('Y') ;?>.&nbsp;&nbsp;All Rights Reserved.</p>
+                <p >Created & Designed by 110 118 & 123</p>
+			</div>
+		</div>
+
+
+	</div>
+</footer>
